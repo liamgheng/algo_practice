66089dcd165c80e0a28f23f9ab4306c53d5e01fb
guoheng <ghsdnu@gmail.com>
2022-02-08 11:38:14 +0800

    链表相交改进版本

--- algo_160.go
+++ algo_160.go
@@ -3,39 +3,20 @@ package algo_practice
 // 10:47:10:59
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
 	headAHash := map[*ListNode]struct{}{}
-	headBHash := map[*ListNode]struct{}{}
 	headACurrent := headA
-	headBCurrent := headB
-	for headACurrent != nil || headBCurrent != nil {
-		if headACurrent == nil {
-			// 只处理 B
-			if _, ok := headAHash[headBCurrent]; ok {
-				return headBCurrent
-			}
-			headBCurrent = headBCurrent.Next
-			continue
-		}
 
-		if headBCurrent == nil {
-			//  只处理 A
-			if _, ok := headBHash[headACurrent]; ok {
-				return headACurrent
-			}
-			headACurrent = headACurrent.Next
-			continue
-		}
-
-		// 两者都没结束，也没相交
+	for headACurrent != nil {
 		headAHash[headACurrent] = struct{}{}
-		headBHash[headBCurrent] = struct{}{}
+		headACurrent = headACurrent.Next
+	}
+
+	headBCurrent := headB
+	for headBCurrent != nil {
 		if _, ok := headAHash[headBCurrent]; ok {
 			return headBCurrent
 		}
-		if _, ok := headBHash[headACurrent]; ok {
-			return headACurrent
-		}
-		headACurrent = headACurrent.Next
 		headBCurrent = headBCurrent.Next
 	}
+
 	return nil
 }
